c12149a810d1d0ef5a789488f75f2bb073535043
Denis Costello <G00396309@atu.ie>
2022-12-07 12:42:32 +0000

    Employee list

---
+++ src/main/java/ie/atu/EmployeeList.java
@@ -0,0 +1,17 @@
+package ie.atu;
+
+import java.util.ArrayList;
+
+public class EmployeeList {
+
+    public EmployeeList() {
+    }
+
+    public static void main(String[] args)
+    {
+        ArrayList<Employee> employelist = new ArrayList<Employee>(2);
+        Employee newEmployee = new Employee("Denis1", "432156");
+
+    }
+
+}
